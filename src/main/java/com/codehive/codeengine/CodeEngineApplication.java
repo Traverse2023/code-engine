8577a6d34393b39f0dc9f1b14f811600a34533c3
bryan <briplomo@gmail.com>
2024-12-17 13:59:40 -0600

    Working checkstyle

--- src/main/java/com/codehive/codeengine/CodeEngineApplication.java
+++ src/main/java/com/codehive/codeengine/CodeEngineApplication.java
@@ -1,13 +1,15 @@
+/**
+ * Code engine api @author Bryan Palomo.
+ * */
 package com.codehive.codeengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CodeEngineApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CodeEngineApplication.class, args);
-	}
-
+    public static void main(final String[] args) {
+        SpringApplication.run(CodeEngineApplication.class, args);
+    }
 }
